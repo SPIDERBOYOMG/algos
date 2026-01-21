45764d9d21adac75079fd96d0c58a51f846a4478
SPIDERBOYOMG <imangalierden2000@gmail.com>
2026-01-21 11:36:47 +0500

    add_floyd

---
+++ floyd.cpp
@@ -0,0 +1,14 @@
+vector<vll> dist(n+1, vll(n+1, inf));
+for(int i=1; i<=n; i++){
+	for(int j=1; j<=n; j++){
+		dist[i][j]=a[i][j];
+	}
+}
+
+for(int k=1; k<=n; k++){
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
+		}
+	}
+}
