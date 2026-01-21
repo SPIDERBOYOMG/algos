0059a9778148ca08b0d1c5b4aa879160e41d1ad0
SPIDERBOYOMG <imangalierden2000@gmail.com>
2026-01-21 11:34:18 +0500

    add_djeicstra

---
+++ Djeistra.cpp
@@ -0,0 +1,15 @@
+for(int i=1; i<=n; i++){
+	int v=-1;
+	
+	for(int j=1; j<=n; j++){
+		if(!was[j] and (v==-1 or dist[j]>dist[v])){
+			v=j;
+		}
+	}
+	
+	was[v]=1;
+	
+	for(auto &[to, len]:g[v]){
+		dist[to]=max(dist[to], dist[v]*len);
+	}
+}
