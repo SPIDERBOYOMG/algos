bd93b4ebe00a5e252a4cfe2f36829379da868250
SPIDERBOYOMG <133570434+SPIDERBOYOMG@users.noreply.github.com>
2026-09-04 12:05:50 +0500

    Create tuntemplate.cpp

---
+++ tuntemplate.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using sixseven = vector<int>;
+using mutabilnyi = string;
+using tuntunsahur = int;
+using tralalelotralala = long long;
+using bez_mam_bez_pap = void;
+using ochered = queue<tralalelotralala>;
+using blatnaya_ochered = deque<tralalelotralala>;
+using batya_recursii = stack<tralalelotralala>;
+#define bombordiloCrocodilo cout
+#define chitai cin
+#define print(x) \
+  for (auto& it : x) cout << it << " "
+using SixSeven = vector<tralalelotralala>;
+#define malenkimi_shazaohkami(l, r, x) for (tuntunsahur i = l; i <= r; i += x)
+#define klinus true
+#define lgu false
+#define ili or
+#define eshe and
+#define beskonechnost_ne_predel while (klinus)
+#define uslovie_1 if
+#define uslovie_2 else if
+#define nu_ostalos else
+#define stop_mne_ne_priyatno break
+#define in :
+#define optimus auto
+using parochka_tral = pair<tralalelotralala, tralalelotralala>;
+using parochka_tuntun = pair<tuntunsahur, tuntunsahur>;
+
+const tuntunsahur dolzhnoHvatit = 2e5 + 67;
+const tralalelotralala CenzuraModul = 5e8 + 5e8 + 7;
+const tuntunsahur miniModul = CenzuraModul - 1755647;
+
+bez_mam_bez_pap reshaisa_pzh() {}
+
+tuntunsahur main() {
+  tuntunsahur ttt = 1;
+  chitai >> ttt;
+  malenkimi_shazaohkami(1, ttt, 1) {
+    reshaisa_pzh();
+    bombordiloCrocodilo << "\n";
+  }
+}
